ecb8a810b3a1db6a3e7a56bab46dbd9dcd9ec582
tynydeedev <88718743+tynydeedev@users.noreply.github.com>
2021-09-13 17:24:58 +0000

    fix the coordinate error in /api/check/ using toUpperCase() with risk of undefined, changed

--- routes/api.js
+++ routes/api.js
@@ -10,8 +10,10 @@ module.exports = function (app) {
   app.route('/api/check')
     .post((req, res) => {
       const puzzle = req.body.puzzle;
-      const coordinate = req.body.coordinate.toUpperCase();
+      const coordinate = req.body.coordinate;
       const value = req.body.value;
+
+      console.log(req.body);
 
       if (!puzzle || !coordinate || !value) return res.json({ error: 'Required field(s) missing' });
 
@@ -22,12 +24,14 @@ module.exports = function (app) {
           return res.json({ error: 'Invalid characters in puzzle' });
       };
 
-      if (!/^[A-I][1-9]$/.test(coordinate)) return res.json({ error: 'Invalid coordinate' });
+      if (!/^[A-I][1-9]$/i.test(coordinate)) return res.json({ error: 'Invalid coordinate' });
 
       if (!/^[1-9]$/.test(value)) return res.json({ error: 'Invalid value' });
 
-      const position = coorConverter(coordinate);
+      const position = coorConverter(coordinate.toUpperCase());
       const puzzleMap = solver.createMap(puzzle);
+      if (puzzleMap[position.y][position.x] === value) return res.json({ valid: true });
+      if (puzzleMap[position.y][position.x] !== '.' && puzzleMap[position.y][position.x] !== value) return res.json({ valid: false })
       const checkRow = solver.checkRowPlacement(puzzleMap, position.y, position.x, value);
       const checkCol = solver.checkColPlacement(puzzleMap, position.y, position.x, value); 
       const checkRegion = solver.checkRegionPlacement(puzzleMap, position.y, position.x, value);
